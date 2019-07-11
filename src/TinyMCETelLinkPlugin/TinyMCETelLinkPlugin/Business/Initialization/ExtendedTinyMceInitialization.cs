f78af693ccd38afa0fe54a5957c757a38cf7909c
anders.jacobsen <anders.jacobsen@valtech.dk>
2019-07-11 15:49:56 +0200

    Added

---
+++ src/TinyMCETelLinkPlugin/TinyMCETelLinkPlugin/Business/Initialization/ExtendedTinyMceInitialization.cs
@@ -0,0 +1,36 @@
+﻿using EPiServer.Cms.TinyMce.Core;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TinyMCETelLinkPlugin.Business.Initialization
+{
+	[ModuleDependency(typeof(TinyMceInitialization))]
+	public class ExtendedTinyMceInitialization : IConfigurableModule
+	{
+		public void Initialize(InitializationEngine context)
+		{
+		}
+
+		public void Uninitialize(InitializationEngine context)
+		{
+		}
+
+		public void ConfigureContainer(ServiceConfigurationContext context)
+		{
+			context.Services.Configure<TinyMceConfiguration>(config =>
+			{
+				// Add content CSS to the default settings.
+				config.Default()
+					.ContentCss("/static/css/editor.css")
+					.AddPlugin("code")
+					.AddExternalPlugin("tellink", "/ClientResources/Scripts/tinymce/plugins/tellink/myplugin.js")
+					.Toolbar(DefaultValues.Toolbar + " | code tellink");
+			});
+		}
+	}
+}
